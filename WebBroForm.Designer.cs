b27ccd114c0f9f7b09fc49803050614f305ca981
JeffDChapman <jeffdchapman@hotmail.com>
2017-03-20 08:18:27 -0700

    Working Flash Screen
    Hide the browser window while the scripts and replacements run

---
+++ WebBroForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WebLoader
+{
+    partial class WebBroForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebBroForm));
+            this.myBrowser = new System.Windows.Forms.WebBrowser();
+            this.myAddrBar = new System.Windows.Forms.TextBox();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnGoTo = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lboxRecent = new System.Windows.Forms.ListBox();
+            this.lblWorking = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // myBrowser
+            // 
+            this.myBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myBrowser.Location = new System.Drawing.Point(-5, 25);
+            this.myBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.myBrowser.Name = "myBrowser";
+            this.myBrowser.ScriptErrorsSuppressed = true;
+            this.myBrowser.Size = new System.Drawing.Size(799, 613);
+            this.myBrowser.TabIndex = 0;
+            this.myBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.myBrowser_DocumentCompleted);
+            this.myBrowser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.myBrowser_Navigating);
+            // 
+            // myAddrBar
+            // 
+            this.myAddrBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myAddrBar.Location = new System.Drawing.Point(28, 0);
+            this.myAddrBar.Name = "myAddrBar";
+            this.myAddrBar.Size = new System.Drawing.Size(711, 20);
+            this.myAddrBar.TabIndex = 1;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(28, 20);
+            this.btnHome.TabIndex = 2;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnGoTo
+            // 
+            this.btnGoTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGoTo.Image = ((System.Drawing.Image)(resources.GetObject("btnGoTo.Image")));
+            this.btnGoTo.Location = new System.Drawing.Point(739, 0);
+            this.btnGoTo.Name = "btnGoTo";
+            this.btnGoTo.Size = new System.Drawing.Size(28, 20);
+            this.btnGoTo.TabIndex = 3;
+            this.btnGoTo.UseVisualStyleBackColor = true;
+            this.btnGoTo.Click += new System.EventHandler(this.btnGoTo_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.Image = ((System.Drawing.Image)(resources.GetObject("btnBack.Image")));
+            this.btnBack.Location = new System.Drawing.Point(766, 0);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(28, 20);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lboxRecent
+            // 
+            this.lboxRecent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lboxRecent.FormattingEnabled = true;
+            this.lboxRecent.Location = new System.Drawing.Point(397, 0);
+            this.lboxRecent.Name = "lboxRecent";
+            this.lboxRecent.Size = new System.Drawing.Size(396, 173);
+            this.lboxRecent.TabIndex = 5;
+            this.lboxRecent.Visible = false;
+            this.lboxRecent.Click += new System.EventHandler(this.lboxRecent_Click);
+            // 
+            // lblWorking
+            // 
+            this.lblWorking.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWorking.AutoSize = true;
+            this.lblWorking.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWorking.Location = new System.Drawing.Point(577, 25);
+            this.lblWorking.Name = "lblWorking";
+            this.lblWorking.Size = new System.Drawing.Size(190, 37);
+            this.lblWorking.TabIndex = 6;
+            this.lblWorking.Text = "Working . . .";
+            // 
+            // WebBroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 638);
+            this.Controls.Add(this.lboxRecent);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnGoTo);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.myAddrBar);
+            this.Controls.Add(this.myBrowser);
+            this.Controls.Add(this.lblWorking);
+            this.Name = "WebBroForm";
+            this.Text = "[Title Goes Here]";
+            this.Load += new System.EventHandler(this.WebBroForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser myBrowser;
+        private System.Windows.Forms.TextBox myAddrBar;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnGoTo;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.ListBox lboxRecent;
+        private System.Windows.Forms.Label lblWorking;
+    }
+}
+
---
+++ WebBroForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebLoader
+{
+    public partial class WebBroForm : Form
+    {
+        public WebBroForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool tbSkipNav = true;
+        private string homeLoc = "file:///C:/Users/jchapman/Desktop/Basics/Work%20Favorites.htm";
+
+        private void WebBroForm_Load(object sender, EventArgs e)
+        {
+            this.btnBack.Enabled = false;
+            this.myBrowser.Navigate(homeLoc);
+        }
+
+        private void myBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlDocument fixDoc = myBrowser.Document;
+            string pageBodyMod = fixDoc.Body.InnerHtml.ToString();
+            string StopRecur = pageBodyMod.Substring(1, 4).ToLower();
+            if (StopRecur == "font")
+            {
+                this.myBrowser.Visible = true;
+                return; 
+            }
+            pageBodyMod = pageBodyMod.Replace("font", "fnot");
+            pageBodyMod = pageBodyMod.Replace("FONT", "FNOT");
+            pageBodyMod = pageBodyMod.Replace("widt", "wdit");
+            pageBodyMod = pageBodyMod.Replace("WIDT", "WDUT");
+            pageBodyMod = pageBodyMod.Replace("styl", "stly");
+            pageBodyMod = pageBodyMod.Replace("STYL", "STLY");
+            pageBodyMod = pageBodyMod.Replace("scri", "srci");
+            pageBodyMod = pageBodyMod.Replace("SCRI", "SRCI");
+            pageBodyMod = pageBodyMod.Replace("Scri", "Srci");
+            pageBodyMod = pageBodyMod.Replace("java", "jav");
+            pageBodyMod = pageBodyMod.Replace("JAVA", "JAV");
+            pageBodyMod = pageBodyMod.Replace("func", "fucn");
+            pageBodyMod = pageBodyMod.Replace("FUNC", "FUCN");
+            pageBodyMod = pageBodyMod.Replace("over", "ovre");
+            pageBodyMod = pageBodyMod.Replace("OVER", "OVRE");
+            pageBodyMod = pageBodyMod.Replace("mouseout", "mouesout");
+            pageBodyMod = pageBodyMod.Replace("MOUSEOUT", "MOUESOUT");
+            pageBodyMod = pageBodyMod.Replace("widg", "wigd");
+            pageBodyMod = pageBodyMod.Replace("WIDG", "WIGD");
+            pageBodyMod = pageBodyMod.Replace("onload", "onlaod");
+            string pageBodyModshow = "<font face=\"verdana\">" + pageBodyMod;
+
+            this.Text = myBrowser.DocumentTitle;
+            myBrowser.Document.OpenNew(false);
+            myBrowser.Document.Write(pageBodyModshow);
+            this.myBrowser.Visible = true;
+            myBrowser.Refresh();
+            this.myAddrBar.Text = myBrowser.Url.ToString();
+        }
+
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            this.myBrowser.Navigate(homeLoc);
+        }
+
+        private void btnGoTo_Click(object sender, EventArgs e)
+        {
+            this.btnBack.Enabled = true;
+            this.lboxRecent.Items.Insert(0, myAddrBar.Text);
+            myBrowser.Navigate(myAddrBar.Text);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.lboxRecent.Visible = true;
+        }
+
+        private void myBrowser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            string reDirLoc = e.Url.ToString();
+            this.myBrowser.Visible = false;
+            if (reDirLoc.Substring(0,6) == "about:")
+            {
+                string baseAddr = myAddrBar.Text;
+                int lastSlashLoc = baseAddr.LastIndexOf("/");
+                if ((reDirLoc.Substring(6, 1) == "/") && (baseAddr.IndexOf(reDirLoc.Substring(6, 4)) >= 0))
+                {
+                    string shorterBase = baseAddr.Substring(0, lastSlashLoc + 1);
+                    lastSlashLoc = shorterBase.LastIndexOf("/") - 1;
+                }
+                string newBaseAddr = baseAddr.Substring(0, lastSlashLoc + 1);
+                string reDirect = newBaseAddr + reDirLoc.Substring(6);
+                if (reDirect.IndexOf("blank") < 0) 
+                {
+                    myAddrBar.Text = reDirect;
+                }
+            }
+        }
+
+        private void lboxRecent_Click(object sender, EventArgs e)
+        {
+            string goToPage = this.lboxRecent.SelectedItem.ToString();
+            this.lboxRecent.Visible = false;
+            myBrowser.Navigate(goToPage);
+        }
+    }
+}
