5e6304d4d47b6a782214138a36130b55ccdd3cfb
JeffDChapman <jeffdchapman@hotmail.com>
2025-02-07 12:42:21 -0800

    Fix Close
    Enable close button on new form

--- EditFavDesc.Designer.cs
+++ EditFavDesc.Designer.cs
@@ -59,6 +59,7 @@
             btnOK.TabIndex = 2;
             btnOK.Text = "OK";
             btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += btnOK_Click;
             // 
             // EditFavDesc
             // 
--- EditFavDesc.cs
+++ EditFavDesc.cs
@@ -16,5 +16,10 @@ namespace WebLoader
         {
             InitializeComponent();
         }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
